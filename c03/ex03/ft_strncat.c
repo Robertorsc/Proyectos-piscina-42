78a8c5671f3650962598835f9bb0a2348b05b761
Robertorsc <robertosaizrsc@gmail.com>
2024-07-24 12:11:23 +0200

    Add files via upload

--- c03/ex03/ft_strncat.c
+++ c03/ex03/ft_strncat.c
@@ -6,22 +6,22 @@
 /*   By: rsaiz-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 08:32:45 by rsaiz-ca          #+#    #+#             */
-/*   Updated: 2024/07/23 13:41:55 by rsaiz-ca         ###   ########.fr       */
+/*   Updated: 2024/07/24 12:03:33 by rsaiz-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strncat(char *dest, char *src, unsigned int n)
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	unsigned int	i;
 	unsigned int	j;
 
 	i = 0;
 	j = 0;
-	while (dest[i] != '\0' && i < n)
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (src[j] != '\0' && i < n)
+	while (src[j] != '\0' && j < nb)
 	{
 		dest[i] = src[j];
 		i++;
